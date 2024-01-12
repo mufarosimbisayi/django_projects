0f9026467ca6124f875586723b9804c9877decc9
msimbisayi <msimbisayi@deloittece.com>
2024-01-12 19:32:34 +0100

    started a news app and created a custom model and added a custom user change and user creation forms to the custom user admin.

---
+++ news/accounts/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    form = CustomUserChangeForm
+    add_form = CustomUserCreationForm
+    list_display = ["email", "username", "age", "is_staff"]
+    fieldsets = UserAdmin.fieldsets + ((None, {"fields": ("age",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + ((None, {"fields": ("age",)}),)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)

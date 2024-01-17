6d61d59ec28ff30d979117f9a6472a948029b511
msimbisayi <msimbisayi@deloittece.com>
2024-01-17 20:07:15 +0100

    added functionality, pages and tests for signup, login and logout pages.

--- news/accounts/views.py
+++ news/accounts/views.py
@@ -7,4 +7,4 @@ from .forms import CustomUserCreationForm
 class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     template_name = 'registration/signup.html'
-    success_url = reverse_lazy('signup')
+    success_url = reverse_lazy('login')
